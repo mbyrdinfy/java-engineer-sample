81e776985d5afee810570f5f535a6dc60b526bb9
matthew.byrd <matthew.byrd@ad.infosys.com>
2019-06-06 13:05:01 -0500

    add given project without edits

---
+++ src/test/java/com/att/demo/integration/AccountServiceIT.java
@@ -0,0 +1,38 @@
+package com.att.demo.integration;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.att.demo.model.Account;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class AccountServiceIT {
+	
+	private String uri ="/accounts";
+		
+	@Before
+	public void setUp() throws Exception {
+		String baseURI = System.getProperty("BASE_URL");
+		RestAssured.baseURI = baseURI + "/api";
+	}
+	
+	private RequestSpecification givenBaseSpec() {
+		return 
+				RestAssured.given()
+					.accept(ContentType.JSON)
+					.contentType(ContentType.JSON);
+	}
+	
+	@Test
+	public void testfindAllAccount_success() {
+		
+		givenBaseSpec()
+				.when()
+				.get(uri)
+				.then()
+					.statusCode(200);
+	}
+}
